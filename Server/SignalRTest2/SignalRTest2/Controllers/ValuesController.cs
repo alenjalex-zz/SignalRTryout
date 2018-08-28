1e753c8fc29a9b61adac154c591f74e86c9cdb28
alenjalex <alen.alex@outlook.com>
2018-08-28 18:48:38 +0530

    Adding SignalR server code

---
+++ Server/SignalRTest2/SignalRTest2/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SignalRTest2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private IHubContext<TimeHub> _hub;
+
+        public ValuesController(IHubContext<TimeHub> hub)
+        {
+            _hub = hub;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            await _hub.Clients.All.SendAsync("Notify", $"Service invoked at {DateTime.Now.ToString()}");
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Server/SignalRTest2/SignalRTest2/Hub/TimeHub.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalRTest2
+{
+    public class TimeHub : Hub
+    {
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("Connected to SignalR Server");
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task Notify(string message)
+        {
+            await Clients.All.SendAsync(message);
+        }
+    }
+}
