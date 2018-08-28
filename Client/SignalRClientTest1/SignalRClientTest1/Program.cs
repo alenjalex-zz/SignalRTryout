01829a92a9b452e8db55909d9dc920f4d11ee2bd
alenjalex <alen.alex@outlook.com>
2018-08-28 18:54:14 +0530

    Adding SignalR client console application

---
+++ Client/SignalRClientTest1/SignalRClientTest1/Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace SignalRClientTest1
+{
+    using Microsoft.AspNetCore.SignalR.Client;
+    using System;
+
+    class Program
+    {
+        private static readonly Action<string> OnReceivedAction = OnReceived;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Connecting...");
+            Connect();
+            Console.ReadLine();
+        }
+
+        private static async void Connect()
+        {
+            var hubConnectionBuilder = new HubConnectionBuilder();
+
+            var hubConnection = hubConnectionBuilder.WithUrl("http://localhost:56349/timeHub").Build();
+
+            await hubConnection.StartAsync();
+            Console.WriteLine("Connected");
+            var on = hubConnection.On("ReceiveMessage", OnReceivedAction);
+
+            Console.WriteLine("Receiving");
+
+            Console.ReadLine();
+            Console.WriteLine("Done..");
+
+            on.Dispose();
+            await hubConnection.StopAsync();
+        }
+
+        static void OnReceived(string message)
+        {
+            Console.WriteLine($"{message}");
+        }
+    }
+}
